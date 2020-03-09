942c91bddb239e6a7280f736697cb2cdcb7ac590
Igor <Beliy>
2020-03-09 16:12:09 +0600

    *Сделан функционал показа случайного числа *Прописана анимация для смены чисел

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 var app = new Vue({
-    el: '#app',
-    data: {
-      number: 0
-    },
-    methods:{
+	el: '#app',
+	data: {
+		number: 1,
+		min:1,
+		max:100,
+	},
+	methods:{
+		getRandomInt() {
+			max  = parseInt(this.max) +1
+			return Math.random() * (max - this.min) + this.min;
+		},
+		clickHandler(){
+			let changeAnimation =  setInterval(()=>{
+				this.number = parseInt(this.getRandomInt())
+			},50)
 
-    }
+			setTimeout(()=>{
+				clearInterval(changeAnimation)
+				this.number = parseInt(this.getRandomInt())
+			},2000)
+			
+		}
+	}
   })
